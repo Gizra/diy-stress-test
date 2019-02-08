875a64641d0e25bb0949486a56d9a207f25639de
Aron Novak <aron@gizra.com>
2019-02-08 11:12:23 +0100

    locally working WDIO test to stream podcast like a real user

--- specs/test.js
+++ specs/test.js
@@ -4,5 +4,17 @@ describe('homepage', () => {
     it('should have the right title', () => {
         browser.url('/');
         $('.contact .btn').click();
+
+        browser.url('/team');
+        const menu = $('.header.menu .fa-bars');
+        menu.waitForDisplayed();
+        menu.click();
+        $('a=Jobs').click();
+        menu.waitForDisplayed();
+        menu.click();
+        $('a=Podcast').click();
+        $('#mep_0 .mejs__controls').waitForDisplayed();
+        $('#mep_0 .mejs__play button').click();
+        $('span=00:05').waitForDisplayed();
     });
 });
